b06e0c758c48f5d11bdad74d14832b91c43b68ce
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-05-11 01:10:50 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+var newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "<h1>YOUR_NAME is the champion</h1>"
